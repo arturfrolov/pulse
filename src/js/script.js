d239e0dea309718181054814d3ddf3941a9866aa
Artur <djajikamail@gmail.com>
2022-08-11 15:49:04 +0300

    add section catalog and reviews, adapted all the sections

--- src/js/script.js
+++ src/js/script.js
@@ -22,7 +22,14 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: true,
+    touch: true,
+    navPosition: top,
+    responsive: {
+      992: {
+        nav: false
+      },
+    }
 });
 
 document.querySelector('.prev').addEventListener('click', function () {
@@ -32,3 +39,40 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });
+
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  // $('.catalog-item__link').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   })
+  // });
+
+  // $('.catalog-item__back').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   })
+  // });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
